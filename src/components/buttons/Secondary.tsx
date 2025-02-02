046898351af59d7fe44291fc3ca4be2fbe612194
Derek Brown <derek@brown.lt>
2025-02-02 02:54:04 -0600

    Disabled; Danger

--- src/components/buttons/Secondary.tsx
+++ src/components/buttons/Secondary.tsx
@@ -11,13 +11,13 @@ interface SecondaryProps {
 }
 
 export function Secondary({ caption = 'Secondary', className = '', icon, type = 'default', key = '', onClick }: SecondaryProps) {
-  const baseClasses = 'text-xs uppercase font-mono transition-all flex items-center gap-1 px-3 py-1 rounded-xl duration-200 hover:-rotate-1 hover:scale-110 active:scale-95';
+  const baseClasses = `text-xs uppercase font-mono transition-all flex items-center gap-1 px-3 py-1 rounded-xl duration-200 ${type !== 'disabled' ? 'hover:-rotate-1 hover:scale-110 active:scale-95' : 'cursor-not-allowed'}`;
 
   const typeClasses = {
     default: 'text-emerald-600 hover:bg-emerald-600 hover:text-white',
     warn: 'text-amber-600 hover:bg-amber-600 hover:text-white',
-    disabled: 'text-stone-600 hover:bg-stone-600 hover:text-white',
-    danger: 'text-red-600 hover:bg-red-600 hover:text-white',
+    disabled: 'bg-stone-200 text-stone-500',
+    danger: 'text-rose-600 hover:bg-rose-600 hover:text-white',
   }[type];
 
   return (
