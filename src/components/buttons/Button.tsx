97acd0736a9c086daf8f9c497e5fadbcf748fd83
Derek Brown <derek@brown.lt>
2025-02-02 12:39:59 -0600

    [Button] Add Disabled, dotColor

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -4,23 +4,25 @@ import { motion } from 'framer-motion';
 interface ButtonProps {
   caption: React.ReactNode;
   className?: string;
+  disabled?: boolean;
+  dotColor?: string;
   key?: string;
   isSelected?: boolean;
   onClick?: () => void;
 }
 
-export function Button({ caption = 'Button', className = '', isSelected = false, key = '', onClick }: ButtonProps) {
+export function Button({ caption = 'Button', className = '', disabled = false, dotColor = '#239821', isSelected = false, key = '', onClick }: ButtonProps) {
   const baseClasses =
     'relative flex bg-stone-50 text-stone-600 font-mono uppercase text-xs rounded-lg items-center justify-center overflow-hidden gap-4 group h-8 px-6 my-2 duration-200';
   const stateClasses = isSelected ? 'scale-90 shadow-inner' : 'hover:scale-90 active:scale-90 active:shadow-inner shadow-md';
 
   return (
-    <button key={key} className={`${baseClasses} ${stateClasses} ${className}`} onClick={onClick}>
+    <button key={key} disabled={disabled} className={`${baseClasses} ${stateClasses} ${className}`} onClick={onClick}>
       <motion.div
         className="absolute left-0 flex h-8 rounded-l-lg"
         style={{
           width: `10%`,
-          backgroundImage: `radial-gradient(circle, #239821 1px, transparent 1px)`,
+          backgroundImage: `radial-gradient(circle, ${dotColor} 1px, transparent 1px)`,
           backgroundSize: '4px 4px',
         }}
         initial={{ width: 0 }}
